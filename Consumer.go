3a335e37e2d5422ff1d32dd88ba26b4e921526d7
dev_martin <martin.y@inception.asia>
2021-07-20 20:01:24 +0700

    add consumer and producer function

---
+++ Consumer.go
@@ -0,0 +1,29 @@
+package demokafkagoroutine
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func consume(ctx context.Context) {
+	// initialize a new reader with the brokers and topic
+	// the groupID identifies the consumer and prevents
+	// it from receiving duplicate messages
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{broker1Address, broker2Address, broker3Address},
+		Topic:   topic,
+		GroupID: "my-group",
+	})
+
+	for {
+		// the `ReadMessage` method blocks util we receive the next event
+		msg, err := r.ReadMessage(ctx)
+		if err != nil {
+			panic("could not read message" + err.Error())
+		}
+		// after receiving the message, log its value
+		fmt.Println("received: ", string(msg.Value))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/martinyonathann/demokafka_goroutine
+
+go 1.14
+
+require github.com/segmentio/kafka-go v0.4.17
